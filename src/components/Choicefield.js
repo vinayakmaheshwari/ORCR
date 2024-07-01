1493175f371fbb4685ceb6a201eeada186ac28b2
Vinayak <96346957+vinayakmaheshwari@users.noreply.github.com>
2024-07-01 14:03:55 +0530

    Update Choicefield.js

--- src/components/Choicefield.js
+++ src/components/Choicefield.js
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 // import Typeofinstitute from './Typeofinstitute';
 import { iits, iiits, nits, GFTI, JAC, BITS } from "./Values";
 import Table from "./Table";
+import { data } from "./data_final_(2)";
+
+
 export default function Choicefield() {
   const [rank, setRank] = useState(1);
-  const [category, setCategory] = useState("General");
-  const [typeOfInstituteName, setTypeOfInstituteName] = useState("ALL");
+  const [category, setCategory] = useState("OPEN");
+  const [typeOfInstituteName, setTypeOfInstituteName] = useState("IIT");
   const [exam, setExam] = useState("JEE Advanced");
   const [clgName, setClgName] = useState("ALL");
+  const [pool, setPool] = useState('ALL');
 
   const categorySelect = (event) => {
     setCategory(event.target.value);
@@ -27,15 +31,15 @@ export default function Choicefield() {
     setTypeOfInstituteName(event.target.value);
     setClgName("ALL");
   };
-  
-  if (rank===''){
+
+  if (rank === "") {
     setRank(1);
   }
 
-  console.log(exam);
-  console.log(typeOfInstituteName);
-  console.log(clgName);
-  console.log(rank);
+  // console.log(exam);
+  // console.log(typeOfInstituteName);
+  // console.log(clgName);
+  // console.log(rank);
 
   return (
     <>
@@ -51,9 +55,8 @@ export default function Choicefield() {
                 id="examination"
                 onChange={handleExamChange}
               >
-                <option value="">SELECT</option>
-                <option value="JEE Mains">JEE Mains</option>
                 <option value="JEE Advanced">JEE Advanced</option>
+                <option value="JEE Mains">JEE Mains</option>
                 <option value="BITSAT">BITSAT</option>
               </select>
             </div>
@@ -75,6 +78,7 @@ export default function Choicefield() {
                 id="category"
                 onChange={categorySelect}
               >
+                <option value="ALL">ALL</option>
                 <option value="OPEN">General</option>
                 <option value="OBC-NCL">OBC</option>
                 <option className="genews" value="EWS">
@@ -141,6 +145,35 @@ export default function Choicefield() {
                     return <option value={iit.name}>{iit.shortName}</option>;
                   })}
                 </select>
+              )}
+              
+              {typeOfInstituteName === "ALL" && (
+                <select
+                id="clgName"
+                className="dropdownSelect"
+                onChange={(e) => {
+                  setClgName(e.target.value);
+                }}
+              >
+                {nits.map((nit) => {
+                    return <option>{nit}</option>;
+                  })}
+                  {iiits.map((iiit) => {
+                    if(iiit!="ALL"){
+                    return <option>{iiit}</option>;
+                    }
+                  })}
+                  {GFTI.map((gfti) => {
+                    if(gfti!="ALL"){
+                    return <option>{gfti}</option>;
+                    }
+                    })}
+                  {JAC.map((jac) => {
+                    if(jac.shortName!="ALL"){
+                    return <option value={jac.name}>{jac.shortName}</option>;
+                    }
+                  })}</select>
+
               )}
 
               {typeOfInstituteName === "NIT" && (
@@ -209,31 +242,42 @@ export default function Choicefield() {
             <div id="programInfo">
               <button className="infoButton">Program</button>
               <select id="program" className="dropdownSelect">
-                <option value="">SELECT</option>
+                <option value="ALL">ALL</option>
+                {data.map((datas)=>{
+                  if(datas.Institute===clgName+' '){
+                    return <option value={datas.Academic_Program_Name}>{datas.Academic_Program_Name}</option>
+                  }
+                })}
               </select>
             </div>
             <div id="poolInfo">
               <button className="infoButton">Pool</button>
-              <select id="pool" className="dropdownSelect">
-                <option value="all">ALL</option>
-                <option value="gender-neutral">Gender-Neutral</option>
-                <option value="female">Female-Only</option>
+              <select id="pool" className="dropdownSelect" onChange={(e)=>setPool(e.target.value)}>
+                <option value="ALL">ALL</option>
+                <option value="Gender-Neutral">Gender-Neutral</option>
+                <option value="Female-only (including Supernumerary)">Female-Only</option>
               </select>
             </div>
             <div id="durationInfo">
               <button className="infoButton">Duration</button>
               <select id="duration" className="dropdownSelect">
-                <option value="all">ALL</option>
+                <option value="ALL">ALL</option>
                 <option value="4">4-Year</option>
                 <option value="5">5-Year</option>
               </select>
             </div>
           </div>
-          
         </div>
       </div>
       <div id="table">
-        <Table exam={exam} rank={rank} category={category} typeOfInstituteName={typeOfInstituteName} clgName={clgName}/>
+        <Table
+          exam={exam}
+          rank={rank}
+          category={category}
+          typeOfInstituteName={typeOfInstituteName}
+          clgName={clgName}
+          pool={pool}
+        />
       </div>
     </>
   );
