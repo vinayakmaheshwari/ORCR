39d066a0231bd800a8805f2b5ad5d05cb2bca925
Vinayak <96346957+vinayakmaheshwari@users.noreply.github.com>
2024-07-02 22:21:26 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Footer() {
   return (
     <div id='footer'>
-       <p>This is an Open Source Project ,contribution will be appreciated :D <a id='githubRepo' href='https://github.com/vinayakmaheshwari/ORCR'>Github Repo</a></p>
-       <p id='madeBy'>Made with &hearts; by <a href='https://github.com/vinayakmaheshwari/'>Vinayak</a></p>
+       <p>This is an Open Source Project ,contribution will be appreciated :D <a id='githubRepo' href='https://github.com/vinayakmaheshwari/ORCR' target='_blank' rel="noreferrer">Github Repo</a></p>
+       <p id='madeBy'>Made with &hearts; by <a href='https://github.com/vinayakmaheshwari/' target='_blank' rel="noreferrer">Vinayak</a></p>
       
     </div>
   )
