b52236bf1f6e26395e16fa7886cea0c94d0c93fd
Vinayak <96346957+vinayakmaheshwari@users.noreply.github.com>
2024-07-03 16:44:10 +0530

    Update Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,43 @@
 import React from "react";
 export default function Table(props) {
-  console.log(props.data);
+
   return (
     <div id="tableContainer">
-      {props.data.length > 0 && (
+
+      {props.exam==="BITSAT"  && (
+        <table id="keywords" cellSpacing="0" cellPadding="0">
+        <thead>
+          <tr>
+            <th>Institute</th>
+            <th>Program</th>
+            <th>Duration</th>
+            <th>Degree Type</th>
+            <th>Qouta</th>
+            <th>Seat Type</th>
+            <th>Gender</th>
+            <th>Cutoff Marks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.data.map((datas) => {
+            return (
+              <tr>
+                <td>{datas.Institute}</td>
+                <td>{datas.Academic_Program_Name}</td>
+                <td>{datas.duration}</td>
+                <td>{datas.Degree_Type}</td>
+                <td>{datas.Quota}</td>
+                <td>{datas.Seat_Type}</td>
+                <td>{datas.Gender}</td>
+                <td>{datas.Closing_Rank}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      )}
+
+      {props.exam!=="BITSAT" && (
         <table id="keywords" cellSpacing="0" cellPadding="0">
           <thead>
             <tr>
