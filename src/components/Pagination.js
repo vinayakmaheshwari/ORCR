d862c0b3c58106957cf8c5858b5b9865f2cb0e51
Vinayak <96346957+vinayakmaheshwari@users.noreply.github.com>
2024-07-03 13:42:06 +0530

    Update Pagination.js

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,72 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { advClgs } from "./iits";
 import { mainsClgs } from "./mainsClgs";
+import Table from "./Table";
 
 export default function Pagination(props) {
+  var clgName = props.clgName;
+  var category = props.category;
+  var pool = props.pool;
+  var duration = props.duration;
+  var rank = parseInt(props.rank);
+  var exam = props.exam;
+  var program = props.program;
 
+  let dataTemp = [];
 
-    var clgName = props.clgName
-    var category = props.category
-    var pool = props.pool
-    var duration = props.duration
-    var rank = parseInt(props.rank)
-    var exam = props.exam
-    var program = props.program
-  
-    let dataTemp = []
-  
-    if(exam==="JEE Advanced"){
-      dataTemp = advClgs
-    }
-    else if(exam==="JEE Mains"){
-      dataTemp = mainsClgs
-    }
-    
-    if(clgName){
-      dataTemp = dataTemp.filter((datas)=>
-        datas.Institute===clgName + ' '
-      )
-    }
-    if(category){
-      dataTemp = dataTemp.filter((datas)=>
-        datas.Seat_Type===category
-      )
-    }
-    if(pool){
-      dataTemp = dataTemp.filter((datas)=>
-        datas.Gender===pool
-      )
-    }
-    if(duration){
-      dataTemp = dataTemp.filter((datas)=>
-        datas.duration===duration
-      )
-    }
-    if(program){
-      dataTemp = dataTemp.filter((datas)=>
-        datas.Academic_Program_Name===program
-    )
-    }
-    if(rank){
-      dataTemp = dataTemp.filter((datas)=>
-        datas.Closing_Rank>=rank
-    )
-    }
-    const pages = Math.ceil(dataTemp.length/10)
-    console.log(pages)
-      
+  if (exam === "JEE Advanced") {
+    dataTemp = advClgs;
+  } else if (exam === "JEE Mains") {
+    dataTemp = mainsClgs;
+  }
 
-  const [currPage, setCurrPage] = useState('1');
+  if (clgName) {
+    dataTemp = dataTemp.filter((datas) => datas.Institute === clgName + " ");
+  }
+  if (category) {
+    dataTemp = dataTemp.filter((datas) => datas.Seat_Type === category);
+  }
+  if (pool) {
+    dataTemp = dataTemp.filter((datas) => datas.Gender === pool);
+  }
+  if (duration) {
+    dataTemp = dataTemp.filter((datas) => datas.duration === duration);
+  }
+  if (program) {
+    dataTemp = dataTemp.filter(
+      (datas) => datas.Academic_Program_Name === program
+    );
+  }
+  if (rank) {
+    dataTemp = dataTemp.filter((datas) => datas.Closing_Rank >= rank);
+  }
+  const pages = Math.ceil(dataTemp.length / 10);
+  const [currPage, setCurrPage] = useState("1");
   const rowsPerPage = 10;
-  const [lastRowIndex, setLastRowIndex] = useState(10);
-  const [firstRowIndex, setFirstRowIndex] = useState(0);
+  const lastRowIndex = rowsPerPage * currPage;
+  const firstRowIndex = lastRowIndex - rowsPerPage;
 
-  return (
-    <div>
+  const pageData = dataTemp.slice(firstRowIndex, lastRowIndex);
+  let pageNumber = []
 
 
-      
+  for (let i = 1; i<=pages ; i++){
+    pageNumber.push(i)
+  }
+  return <div id="pagination">
+    <Table data={pageData}/>
+    <div id="paginationButtons">
+    {currPage!=1 && <button className="pageButton" onClick={()=>setCurrPage(1)}>FirstPage</button>}
+      {currPage!=1 && <button className="pageButton" onClick={()=>setCurrPage(currPage-1)}>Previous</button>}
+      <button id="currPage">{currPage} of {pages}</button>
+      {currPage!=pageNumber.length  && <button className="pageButton" onClick={()=>setCurrPage(parseInt(currPage)+1)}>Next</button> }
+      {currPage!=pageNumber.length  && <button className="pageButton" onClick={()=>setCurrPage(pageNumber.length)}>LastPage</button> }
     </div>
-  )
+  </div>;
 }
-
